997366f7e1a8f3d644a6012c797f3ba156ddaf51
AJChestnut <84687845+AJChestnut@users.noreply.github.com>
2021-07-12 17:19:57 -0600

    Create 17 Network Programming.py

---
+++ 17 Network Programming.py
@@ -0,0 +1,12 @@
+# Assignemnt 17:
+
+# This week, you are going to build on the program from last week. Use the same file from last week. 
+# You will be extracting the first partition entry from the master boot record that is contained in 
+# the file. The first partition entry starts at offset 0x1BE and ends at 0x1CD. Pull that chunk of 
+# bytes out of the file provided and send it to the server software that you will write. The server 
+# will listen for the chunk of data and print out the status of the drive, the partition type and 
+# the starting address of the partition as an integer. 
+
+# You can refer to Wikipedia (Links to an external site.) for additional details about offsets within 
+# the master boot record and partition table for the pieces of information you are looking for. 
+
