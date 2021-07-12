37f3b27581f536a08a133ed68945f300aa142cab
AJChestnut <84687845+AJChestnut@users.noreply.github.com>
2021-07-11 21:35:18 -0600

    Update and rename Multiplication Table.py to 09 Multiplication Table.py

--- 09 Multiplication Table.py
+++ 09 Multiplication Table.py
@@ -1,4 +1,4 @@
-# Assignment:
+# Assignment 09:
 # Extra Credit - 5 possible points - Write a program that generates the multiplication 
 # table for numbers 1-10. Use two for loops to complete your program. You will need to 
 # put one for loop inside of the other for loop. As an extra challenge, see if you can 
